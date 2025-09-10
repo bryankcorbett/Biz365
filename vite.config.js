417adc528a4f802602bd036d3ea38a0f586fba6a
percy <bryankcorbett@gmail.com>
2025-09-10 10:29:56 -0400

    Remove plugin causing jsx attribute error

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  plugins: [react(), mode === "development" && screenGraphPlugin()],
+  plugins: [react()],
   publicDir: "./static",
   base: "./",
   css: {
